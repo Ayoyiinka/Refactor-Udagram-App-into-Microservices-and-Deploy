3d7e695e50b510046118a309b47118b7a23d0665
Ayoyinka <ayoyinkaobisesan@gmail.com>
2020-05-12 20:23:43 +0100

    docker-hub-push

--- deployment/docker_deploy.sh
+++ deployment/docker_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
-docker push noahross/udacity-restapi-user
-docker push noahross/udacity-restapi-feed
-docker push noahross/reverseproxy
-docker push noahross/udacity-frontend:local
+docker push 164678/udacity-restapi-user
+docker push 164678/udacity-restapi-feed
+docker push 164678/reverseproxy
+docker push 164678/udacity-frontend:local
